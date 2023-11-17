229e463dc3c9a44ac4f05e2d0f8ad679473d9845
ksuviper <ksuviper@gmail.com>
2023-11-16 23:53:35 -0600

    added initial templates, basic views, and urls

--- homeo/urls.py
+++ homeo/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = "polls"
+app_name = "homeo"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("", views.RemedyView.as_view, name="remedy"),
+    path("", views.MateriaView.as_view, name="materia"),
 ]
--- homeo/views.py
+++ homeo/views.py
@@ -4,6 +4,22 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Choice, Question
+from .models import Remedy, RemedyPotency, RemedyMateriaMedica, Potency, MateriaMedica
+
+
+class IndexView(generic.ListView):
+    template_name = "homeo/index.html"
+    context_object_name = "latest_question_list"
+
+
+class RemedyView(generic.DetailView):
+    model = Remedy
+    template_name = "homeo/remedy.html"
+
+
+class MateriaView(generic.DetailView):
+    model = MateriaMedica
+    template_name = "homeo/materia.html"
+
 
 
