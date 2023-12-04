0eb56255cfa18407fe1cb5628681db1362f22065
ksuviper <ksuviper@gmail.com>
2023-12-04 04:03:59 -0600

    index template updates

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@
 
 # x(5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # x(5/5 points) Proper import of packages used.
-# (70/70 points) Using Django you need to setup the following:
+# x(70/70 points) Using Django you need to setup the following:
 # x(10/10 points) Setup a proper folder structure, use the tutorial as an example. You need a minimum of one app.
 # x(20/20 points) You need to have a minimum of 5 pages, using a proper template structure.
 # x(10/10 points) You need to have at least one page that utilizes a form.
@@ -14,7 +14,7 @@
 #                just make use of the admin system.
 # x(10/10) You need to use Bootstrap in your web templates. I won't dictate exactly what modules you need to use but
 #          the more practice here the better. You need to at least make use of a modal.
-# (10/10) You need to setup some sort of register and login system, do some simple research to find examples.
+# x(10/10) You need to setup some sort of register and login system, do some simple research to find examples.
 # x(5/5 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # x(5/5 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt
 #               file which contains all the packages that need installed. You can create this fille with the output of
