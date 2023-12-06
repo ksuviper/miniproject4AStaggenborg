056206ac1d447c6b6a68dcf3004e082763a3ada5
ksuviper <ksuviper@gmail.com>
2023-12-05 23:29:19 -0600

    UI Tweaks Index page Search final

--- homeo/urls.py
+++ homeo/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "homeo"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", views.index, name="index"),
     path("<int:pk>/", views.RemedyView.as_view(), name="remedy"),
     path("<int:pk>/materia/", views.MateriaView.as_view(), name="materia"),
 ]
--- homeo/views.py
+++ homeo/views.py
@@ -8,17 +8,16 @@ from django.utils import timezone
 from .models import Remedy, MateriaMedica
 
 
-class IndexView(generic.ListView):
-    template_name = "homeo/index.html"
-    context_object_name = "remedy_list"
-
-    if request.method == "POST":
-        data = request.POST
-        q = data.get("query")
-
-    def get_queryset(self):
-        """Return the list of remedies"""
-        return Remedy.objects.select_related().all()
+# class IndexView(generic.ListView):
+#     template_name = "homeo/index.html"
+#     context_object_name = "remedy_list"
+#
+#     def get_queryset(self):
+#         """Return the list of remedies"""
+#         if self.request.GET.get("query") is not None:
+#             q = self.request.GET.get("query")
+#             r = Remedy.objects.select_related().filter(name__startswith=q)
+#         r = Remedy.objects.select_related().all()
 
 
 class RemedyView(generic.DetailView):
@@ -29,6 +28,25 @@ class RemedyView(generic.DetailView):
 class MateriaView(generic.DetailView):
     model = MateriaMedica
     template_name = "homeo/materia.html"
+
+
+# def search(request):
+#     q = request.POST.get("query")
+#     if q is not None:
+#         r = Remedy.objects.filter(name__startswith=q)
+#     return render(request, "homeo/index.html", {"remedy": r})
+
+
+def index(request):
+    """Return the list of remedies"""
+    if request.GET.get("query") is not None:
+        q = request.GET.get("query")
+        r = Remedy.objects.select_related().filter(name__startswith=q)
+    else:
+        q = ""
+        r = Remedy.objects.select_related().all()
+
+    return render(request, "homeo/index.html", {"remedy_list": r, "query": q})
 
 
 def remedy(request, remedy_id):
